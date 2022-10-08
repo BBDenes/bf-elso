a793f2768328ba0ebbd6d16668b6b7209b61ad4f
BBDenes <csennel302@gmail.com>
2022-10-08 21:49:18 +0200

    asd

--- bb_2/index.js
+++ bb_2/index.js
@@ -2,8 +2,12 @@ const t = document.getElementById("puzzle")
 let table;
 let len;
 let canPlace = false;
-var redPlaceable = [];
-var bluePlaceable = [];
+var array = [];
+var canPut = "blue";
+var deff = "red";
+class Cell{
+    constructor(row, col, )
+}
 
 
 function CheckInput() {
@@ -32,8 +36,7 @@ function FillTable(row, col) {
     }
     t.innerHTML = ""
     for (let i = 0; i < row; i++) {
-        let msg = ``;
-        msg += `<tr>`
+        window.array.push([]);
         for (let j = 0; j < col; j++){
             if ((i==startingPosRed[0]&&j==startingPosRed[1]) || (i==startingPosRed[0]+1&&j==startingPosRed[1]+1)) {
                 msg += `<td><span id=${i*col + j} class="red" onmouseover="Check(${i*col + j})" style="height: 30px; width: 30px; background-color: red;border-radius: 50%; display: inline-block;"></span></td>`;
@@ -48,23 +51,37 @@ function FillTable(row, col) {
         msg += `</tr>`;
         t.innerHTML += msg;
         //console.log(document.getElementById(3).innerHTML);
+            window.array[i].push({color : false, y : i, x : j, isItFree : true, worth : 0, selected : false});
+        }
     }
+    array[len/2-1][len/2-1].color = "red";
+    array[len/2][len/2].color  = "red";
+    array[len/2][len/2-1].color  = "blue";
+    array[len/2-1][len/2].color  = "blue";
+    load(array);
     canPlace = true;
-
 }
 
-function Check(index){
-    let current = document.getElementById(index).innerHTML;
-    //console.log(x);
-    let target = {
-        left: document.getElementById(index - 1),
-        right: document.getElementById(index + 1),
-        top: document.getElementById(index - len),
-        bottom: document.getElementById(index + len)
-    };
-    if(target.left.className == "blue"){
-        console.log("Lehet rakni pirosat")
+function put(x,y){
+    console.log(Check(x,y));
+}
+
+function Check(x,y){
+    for (let i = 0; i < window.array.length; i++) {
+        for (let j = 0; j < window.array[i].length; j++){
+            if (window.array[i][j].selected){window.array[i][j].selected = false; document.getElementById(window.array[i][j].id).padding = "2px"}
+        }
     }
+    if (!window.array[x][y].color && ((x > 0 ? window.array[x-1][y].color == window.canPut : false) || (y > 0 ? window.array[x][y-1].color == window.canPut : false) || (x < window.array.length-1 ? window.array[x+1][y].color == window.canPut : false) || (window.array[x].length-1 > y ? window.array[x][y+1].color == window.canPut : false))){
+        for (let i = 0; i < window.array[x].length; i++){if (window.array[x][i].color == deff){window.array[x][y].selected = true;}}
+        for (let i = 0; i < window.array.length; i++){if (window.array[i][y].color == deff){window.array[x][y].selected = true;}}
+        for (let i = x; i < window.array.length; i++){for (let k = y; k < window.array[i].length; k++){if (window.array[i][k].color == deff){window.array[x][y].selected = true;}}}
+        for (let i = x; i > 0; i--){for (let k = y; k > 0; k--){if (window.array[i][k].color == deff){window.array[x][y].selected = true;}}}
+        for (let i = x; i < window.array.length; i++){for (let k = y; k > 0; k--){if (window.array[i][k].color == deff){window.array[x][y].selected = true;}}}
+        for (let i = x; i > 0; i--){for (let k = y; k < window.array[i].length; k++){if (window.array[i][k].color == deff){window.array[x][y].selected = true;}}}
+    }
+    load(window.array);
+    return array[x][y].selected;
 }
 
 function Move(r, c){
